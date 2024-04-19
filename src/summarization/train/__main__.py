6f1e207af184c88dc9141119039d7fef9e361b9a
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-04-20 01:50:24 +0700

    fix

--- src/summarization/train/__main__.py
+++ src/summarization/train/__main__.py
@@ -20,7 +20,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    '--max_epochs', '-me', type=int, default=4,
+    '--max_epochs', '-me', type=int, default=3,
                     help='max epoch')
 parser.add_argument(
     '--batch_size', '-bs', type=int, default=16,
