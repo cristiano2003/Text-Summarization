80b55abcac1cae5ec4bc2b3c7ca789745f82eaf8
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-04-22 09:35:19 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,4 @@
 # install package
 pip install -e .
-wget https://drive.google.com/file/d/1p9gFqHlHiH_5fI9i5I6cD6hGuaPTdNR9/view?usp=share_link
+wget https://drive.usercontent.google.com/download?id=1p9gFqHlHiH_5fI9i5I6cD6hGuaPTdNR9&export=download&authuser=0&confirm=t&uuid=fd13b989-0af0-4d63-9402-46a98bcb8f50&at=APZUnTVCq3-twY9PUbRCFkjHUfo7%3A1713753283031
 unzip Best-T5-epoch=0-step=17500-val_loss=1.54.ckpt.zip 
