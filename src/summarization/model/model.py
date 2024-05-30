a8f08d542b7e70efe4e08982242fd51e078a33ad
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.100>
2024-05-30 09:06:45 +0700

    fix

--- src/summarization/model/model.py
+++ src/summarization/model/model.py
@@ -23,56 +23,56 @@ class NewsSummaryModel(pl.LightningModule):
     
 
   def forward(self, input_ids, attention_mask, decoder_attention_mask, labels = None):
-    output = self.model(
+      output = self.model(
         input_ids,
         attention_mask = attention_mask,
         labels = labels,
         decoder_attention_mask=decoder_attention_mask
     )
-    return output.loss, output.logits
+      return output.loss, output.logits
 
   def training_step(self, batch, batch_idx):
     
-    input_ids = batch["text_input_ids"]
-    attention_mask = batch["text_attention_mask"]
-    labels = batch["labels"]
-    labels_attention_mask = batch["labels_attention_mask"]
+      input_ids = batch["text_input_ids"]
+      attention_mask = batch["text_attention_mask"]
+      labels = batch["labels"]
+      labels_attention_mask = batch["labels_attention_mask"]
 
-    loss, outputs = self(input_ids=input_ids, 
-                         attention_mask=attention_mask,
-                         decoder_attention_mask=labels_attention_mask,
-                         labels=labels
-                         )
-    self.log("train_loss", loss, prog_bar=True, logger=True)
-    return loss
+      loss, outputs = self(input_ids=input_ids, 
+                          attention_mask=attention_mask,
+                          decoder_attention_mask=labels_attention_mask,
+                          labels=labels
+                          )
+      self.log("train_loss", loss, prog_bar=True, logger=True)
+      return loss
 
   def validation_step(self, batch, batch_idx):
-    input_ids = batch["text_input_ids"]
-    attention_mask = batch["text_attention_mask"]
-    labels = batch["labels"]
-    labels_attention_mask = batch["labels_attention_mask"]
+      input_ids = batch["text_input_ids"]
+      attention_mask = batch["text_attention_mask"]
+      labels = batch["labels"]
+      labels_attention_mask = batch["labels_attention_mask"]
 
-    loss, outputs = self(input_ids=input_ids, 
-                         attention_mask=attention_mask,
-                         decoder_attention_mask=labels_attention_mask,
-                         labels=labels
-                         )
-    self.log("val_loss", loss, prog_bar=True, logger=True)
-    return loss
+      loss, outputs = self(input_ids=input_ids, 
+                          attention_mask=attention_mask,
+                          decoder_attention_mask=labels_attention_mask,
+                          labels=labels
+                          )
+      self.log("val_loss", loss, prog_bar=True, logger=True)
+      return loss
   
   def test_step(self, batch, batch_idx):
-    input_ids = batch["text_input_ids"]
-    attention_mask = batch["text_attention_mask"]
-    labels = batch["labels"]
-    labels_attention_mask = batch["labels_attention_mask"]
+      input_ids = batch["text_input_ids"]
+      attention_mask = batch["text_attention_mask"]
+      labels = batch["labels"]
+      labels_attention_mask = batch["labels_attention_mask"]
 
-    loss, outputs = self(input_ids=input_ids, 
-                         attention_mask=attention_mask,
-                         decoder_attention_mask=labels_attention_mask,
-                          labels=labels
-                         )
-    self.log("test_loss", loss, prog_bar=True, logger=True)
-    return loss
+      loss, outputs = self(input_ids=input_ids, 
+                          attention_mask=attention_mask,
+                          decoder_attention_mask=labels_attention_mask,
+                            labels=labels
+                          )
+      self.log("test_loss", loss, prog_bar=True, logger=True)
+      return loss
 
   def configure_optimizers(self):
       return AdamW(self.model.parameters(), lr = 1e-5)
@@ -82,11 +82,11 @@ if __name__ == "__main__":
 
 
 # Load the model and create example input as before
-  model = T5ForConditionalGeneration.from_pretrained('t5-base')
-  tokenizer =  T5Tokenizer.from_pretrained('t5-base')
-  input_ids = tokenizer("This is an example input", return_tensors="pt").input_ids
-  print(input_ids)
-  outputs = model.generate(input_ids)
-  print(outputs)
-  # Print the model summary
-  print(tokenizer.decode(outputs, skip_special_tokens = True))
+    model = T5ForConditionalGeneration.from_pretrained('t5-base')
+    tokenizer =  T5Tokenizer.from_pretrained('t5-base')
+    input_ids = tokenizer("This is an example input", return_tensors="pt").input_ids
+    print(input_ids)
+    outputs = model.generate(input_ids)
+    print(outputs)
+    # Print the model summary
+    print(tokenizer.decode(outputs, skip_special_tokens = True))
