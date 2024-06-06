2a075d8064cab86dbfe19a52a14f3da85802f8e7
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.104>
2024-06-06 14:53:31 +0700

    fix

--- src/summarization/train/__main__.py
+++ src/summarization/train/__main__.py
@@ -82,8 +82,8 @@ def train(model_name):
             tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small", model_max_length=512)
             model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
         else:
-            tokenizer = T5Tokenizer.from_pretrained("lucadiliello/bart-small", model_max_length=512)
-            model = T5ForConditionalGeneration.from_pretrained("lucadiliello/bart-small")
+            tokenizer = BartTokenizer.from_pretrained("facebook/bart-small", model_max_length=512)
+            model = BartForConditionalGeneration.from_pretrained("facebook/bart-small")
             
     else:
         if model_name == 't5':
