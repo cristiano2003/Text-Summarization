303901e24a41ed770a72e02e4f8381b364b8f996
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.104>
2024-06-08 23:00:23 +0700

    evaluate

--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,4 @@
 # install package
 pip install -e .
-wget https://drive.usercontent.google.com/download?id=1p9gFqHlHiH_5fI9i5I6cD6hGuaPTdNR9&export=download&authuser=0&confirm=t&uuid=fd13b989-0af0-4d63-9402-46a98bcb8f50&at=APZUnTVCq3-twY9PUbRCFkjHUfo7%3A1713753283031
-unzip Best-T5-epoch=0-step=17500-val_loss=1.54.ckpt.zip 
+wget "https://drive.usercontent.google.com/download?id=1TKTVRq7Rr5gB5wuZUaHXarGfdCPZD6MJ&export=download&authuser=0&confirm=t&uuid=1c80ca85-142f-414e-b404-e8de38292fdd&at=APZUnTXI5oRsD9x986sQkjLxyUHw%3A1717862378936"
+unzip archive.zip 
