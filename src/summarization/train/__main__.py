bed5b88388448ae44d9de99ba00291b1f28ef74f
cristiano2003 <cristiano2003@example.com>
2025-05-20 17:37:08 +0700

    fix

--- src/summarization/train/__main__.py
+++ src/summarization/train/__main__.py
@@ -132,7 +132,8 @@ def train(model_name):
             callbacks=[checkpoint_callback, lr_callback],
             max_epochs=N_EPOCHS,
             enable_progress_bar=True,
-            log_every_n_steps=500
+            log_every_n_steps=500,
+            val_check_interval=10
         )
         
 
